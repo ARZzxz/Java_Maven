3b6cd8f5ab1c753fb020d1bc822c446fa69f4b73
ARZzxz <achmadrizky0211@gmail.com>
2024-11-09 21:06:52 +0700

    First Commit

---
+++ src/main/java/com/porto/Java/Portofolio/JavaPortofolioApplication.java
@@ -0,0 +1,13 @@
+package com.porto.Java.Portofolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPortofolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPortofolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porto/Java/Portofolio/JavaPortofolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porto.Java.Portofolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPortofolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
